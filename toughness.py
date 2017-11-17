fd1234fd466655540c70f7125ebaaf8167a56dd0
spitzc <33139308+spitzc@users.noreply.github.com>
2017-11-16 21:18:56 -0500

    factors to measure power creep

---
+++ toughness.py
@@ -0,0 +1,9 @@
+import json
+
+with open('AllSets.json', encoding='latin-1') as json_data:
+    all_sets = json.load(json_data)
+    for set_code in all_sets:
+        for a_card in all_sets[set_code]['cards']:
+            if 'toughness' in a_card:
+                print(a_card['name'])
+                print(a_card['toughness'])
