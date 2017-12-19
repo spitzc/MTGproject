54ae38e8067001b7ccec088675628f7853583c7e
spitzc <33139308+spitzc@users.noreply.github.com>
2017-12-18 22:46:45 -0500

    Flavor text from 10 sets
    Lower case flavor text without punctuation from the following sets: Apocalypse (APC), Cold Snap (CSP), Dragons of Tarkir (DTK), Fallen Empires (FEM), Ice Age (ICE), Innistrad (ISD), Khans of Tarkir (KTK), Rturn to Ravnica (RTR), Shards of Alara (ALA), and Zendikar (ZEN).

---
+++ flavorTextZEN.py
@@ -0,0 +1,14 @@
+import json, re
+
+with open('AllSets.json', encoding='latin-1') as json_data:
+    all_sets = json.load(json_data)
+    text_data = []
+    for set_code in all_sets:
+        for a_card in all_sets['ZEN']['cards']:
+            if 'flavor' in a_card:
+                all_set_text = a_card['flavor'].lower()
+                set_text = re.findall(r"[\w']+|[.,!?;]", all_set_text)
+                text_data.append(set_text[0])
+                
+
+json.dump(text_data, open('Zendikar_data.json', 'w'),indent=4)
